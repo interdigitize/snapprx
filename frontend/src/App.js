e44de69f8c45646f0baaa7b7ef1f5edd9da24073
interdigitize <interdigitize@users.noreply.github.com>
2018-02-04 12:01:10 -0800

    UI updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as MyScriptJS from 'myscript/src/myscript'
+import * as MyScriptJS from 'myscript/src/myscript';
 import 'myscript/dist/myscript.min.css';
-
-const editorStyle = {
-  'minWidth': '500px',
-  'minHeight': '500px',
-};
-
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'who'};
+    this.state = { value: 'who' };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,19 +17,23 @@ class App extends Component {
     const self = this;
     const url = `/api/todoc?recipientEmail=${recipient}&data=${rxdata}`;
     console.error('[App.js]', url);
-    return Promise.resolve(true).then(() => {
-      return Promise.resolve(fetch(url));
-    }).then((response) => {
-      return response.json();
-    }).then((data) => {
-      return true;
-    }).catch((ex) => {
-      console.error('[App.js] parsing failed', ex);
-    });
+    return Promise.resolve(true)
+      .then(() => {
+        return Promise.resolve(fetch(url));
+      })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        return true;
+      })
+      .catch(ex => {
+        console.error('[App.js] parsing failed', ex);
+      });
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
   handleSubmit(event) {
@@ -52,26 +50,29 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header id="header">
+          <div>SnappRX</div>
+          <div>Florence Nightingale</div>
         </header>
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Select Doctor:
-            <select value={this.state.value} onChange={this.handleChange}>
-              <option value="franklin">Dr. Franklin</option>
-              <option value="pruim">Dr Pruim</option>
-              <option value="sm94010@gmail.com">Dr. Who</option>
-            </select>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-      <div style={editorStyle} ref="editor">
-
-        </div>
+        <div id="editorStyle" ref="editor" />
+        <footer>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              <span style={{ marginRight: '1rem', marginLeft: '37px' }}>
+                Select Doctor:
+              </span>
+              <select value={this.state.value} onChange={this.handleChange}>
+                <option value="franklin">Dr. Franklin</option>
+                <option value="pruim">Dr Pruim</option>
+                <option value="domore">Dr Domore</option>
+                <option value="sm94010@gmail.com">Dr. Who</option>
+              </select>
+            </label>
+            <button type="submit" style={{ marginLeft: '1rem' }}>
+              Submit
+            </button>
+          </form>
+        </footer>
       </div>
     );
   }
@@ -85,11 +86,13 @@ class App extends Component {
           scheme: 'https',
           host: 'webdemoapi.myscript.com',
           applicationKey: '24795c29-a481-4fe7-9af7-96a8e2aa1bd1',
-          hmacKey: '9170da98-7d01-4c83-bdad-82b03dd52875',
-        },
-      },
+          hmacKey: '9170da98-7d01-4c83-bdad-82b03dd52875'
+        }
+      }
     });
-    window.addEventListener("resize", () => {this.editor.resize()});
+    window.addEventListener('resize', () => {
+      this.editor.resize();
+    });
   }
 }
 
