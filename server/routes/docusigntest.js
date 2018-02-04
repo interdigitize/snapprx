71123542b77cc73a232ece4c944f93e47cf8dec9
Saad Mir <saadmir@gmail.com>
2018-02-04 13:27:48 -0800

    txt msg

--- server/routes/docusigntest.js
+++ server/routes/docusigntest.js
@@ -2,6 +2,53 @@ var     async = require("async"),		// async module
         request = require("request"),		// request module
     	fs = require("fs");			// fs module
 
+      function notify(url, dr) {
+        console.log('[notify]', url, dr);
+        var TeleSignSDK = require('telesignsdk');
+
+        const customerId = "DCE62E42-750D-4B42-B1F0-FABB01817C61"; // Todo: find in portal.telesign.com
+        const apiKey = "vUlYMnvCLjMzfHpSY6q5L5rJ1oVoV4yN7ev2bvtlzDNUTFJYlzf5XxQdZSRMsk9nH703IodqeN/XxiXT6HkR+w=="; // Todo: find in portal.telesign.com
+        const rest_endpoint = "https://rest-api.telesign.com"; // Todo: Enterprise customer, change this!
+        const timeout = 10*1000; // 10 secs
+
+        const client = new TeleSignSDK( customerId,
+            apiKey,
+            rest_endpoint,
+            timeout // optional
+            // userAgent
+        );
+
+        var phoneNumber;
+        var message = 'You have recieved a new perscription to sign  '+url;;
+        const messageType = "ARN";
+        if(dr == 'tom.pruim1@gmail.com'){
+          phoneNumber = 17085190579;
+        }else if (dr == 'frank3562@gmail.com') {
+          phoneNumber = 16303407876;
+        }else if( dr == 'sm94010@gmail.com') {
+          phoneNumber = 14153356477;
+        }else{
+          phoneNumber = 17085190579;
+          message = "The perscription for Paul Zipser has been filled at Fred's Pharmacy    " +url;
+        }
+
+        console.log("## MessagingClient.message ##");
+
+        function messageCallback(error, responseBody) {
+
+            console.log('callback');
+            if (error === null) {
+                console.log(`Messaging response for messaging phone number: ${phoneNumber}` +
+                    ` => code: ${responseBody['status']['code']}` +
+                    `, description: ${responseBody['status']['description']}`);
+            } else {
+                console.error("Unable to send message. " + error);
+            }
+        }
+
+        client.sms.message(messageCallback, phoneNumber, message, messageType);
+      };
+
 module.exports = function(recipientName, recipientEmail, documentName, document, cb) {
   console.log('[docusigntest.js]', recipientName, recipientEmail);
 var 	email = process.env.email,				// your account email
@@ -170,6 +217,7 @@ async.waterfall(
 				return;
 			else {
         console.log("\nNavigate to the above URL to start the Embedded Signing workflow...");
+        notify(recipientId, url);
         return cb(JSON.parse(body).url);
       }
 		});
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,7 @@ const docuSignRequest = require('./docusigntest.js');
 
 var fileupload = require("express-fileupload");
 
+
 router.post('/notification', function(req, res, next){
   console.log(req.body);
   console.log(req.query);
@@ -33,8 +34,8 @@ router.post('/todoc', function(req, res, next){
 
         console.log('[index.js]', req.body.recipientName, req.body.recipientEmail);
         docuSignRequest(req.body.recipientName, req.body.recipientEmail, fn, document, function(url) {
-          console.log('URL', url);
-          notify(url, req.body.recipientEmail);
+          console.log('> > >  >URL', url);
+          // notify(url, req.body.recipientEmail);
           res.status(200).send(url);
         });
       });
@@ -92,50 +93,4 @@ router.get('/docusign', function(req, res, next){
   res.send('200');
 });
 
-function notify(url, dr){
-  console.log(url, dr);
-var TeleSignSDK = require('telesignsdk');
-
-const customerId = "DCE62E42-750D-4B42-B1F0-FABB01817C61"; // Todo: find in portal.telesign.com
-const apiKey = "vUlYMnvCLjMzfHpSY6q5L5rJ1oVoV4yN7ev2bvtlzDNUTFJYlzf5XxQdZSRMsk9nH703IodqeN/XxiXT6HkR+w=="; // Todo: find in portal.telesign.com
-const rest_endpoint = "https://rest-api.telesign.com"; // Todo: Enterprise customer, change this!
-const timeout = 10*1000; // 10 secs
-
-const client = new TeleSignSDK( customerId,
-    apiKey,
-    rest_endpoint,
-    timeout // optional
-    // userAgent
-);
-
-var phoneNumber;
-var message = 'You have recieved a new perscription to sign  '+url;;
-const messageType = "ARN";
-if(dr == 'tom.pruim1@gmail.com'){
-  phoneNumber = 17085190579;
-}else if (dr == 'frank3562@gmail.com') {
-  phoneNumber = 16303407876;
-}else if( dr == 'sm94010@gmail.com') {
-  phoneNumber = 14153356477;
-}else{
-  phoneNumber = 17085190579;
-  message = "The perscription for Paul Zipser has been filled at Fred's Pharmacy"
-}
-
-console.log("## MessagingClient.message ##");
-
-function messageCallback(error, responseBody) {
-
-    console.log('callback');
-    if (error === null) {
-        console.log(`Messaging response for messaging phone number: ${phoneNumber}` +
-            ` => code: ${responseBody['status']['code']}` +
-            `, description: ${responseBody['status']['description']}`);
-    } else {
-        console.error("Unable to send message. " + error);
-    }
-}
-
-client.sms.message(messageCallback, phoneNumber, message, messageType);
-};
 module.exports = router;
